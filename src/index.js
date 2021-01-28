b7e7f1fa28be81eab37dae532dd39f9ef00e0dbb
michael <mpthornton86@gmail.com>
2021-01-28 15:25:50 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTask = document.getElementById('new-task-description')
+  const form = document.getElementById('create-task-form')
+  const taskBtn = document.getElementById('new-task-btn')
+  const list = document.getElementById('tasks')
+
+  document.addEventListener('click', handleClick)
+
+  function handleClick(e) {
+    if(e.target.id === 'new-task-btn') {
+      addTask(e)
+    }else if(e.target.id === 'delete') {
+      removeTask(e)
+    }
+  }
+
+  function addTask(e) {
+    e.preventDefault()
+    let task = newTask.value
+    list.innerHTML += `<li id="new-task">${task} <button id="delete">Remove Task</button></li>`
+  }
+
+  function removeTask(e) {
+    e.preventDefault()
+    let task = document.getElementById('new-task')
+    task.remove()
+  }
+
 });
